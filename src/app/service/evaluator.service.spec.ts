36b532faefb212166cbad9add29abbbd51af8fd5
Sébastien Roy <sebastienjjroy@gmail.com>
2020-04-27 21:07:03 -0400

    Added more tests for high card in evaluator service

--- src/app/service/evaluator.service.spec.ts
+++ src/app/service/evaluator.service.spec.ts
@@ -113,4 +113,55 @@ describe('EvaluatorService', () => {
     // assert
     expect(result).toBe(-1);
   });
+
+  it('should have player A win the hand with a higher card of straight', () => {
+    // arrange
+    const handA = new Hand();
+    handA.handRank = HandRankType.Straight;
+    handA.highCardKinds = [13];
+    handA.multiplesCardKinds = [];
+    const handB = new Hand();
+    handB.handRank = HandRankType.Straight;
+    handB.highCardKinds = [12];
+    handB.multiplesCardKinds = [];
+
+    // act
+    const result = service.compareHands(handA, handB);
+    // assert
+    expect(result).toBe(1);
+  });
+
+  it('should have split pot with same high cards', () => {
+    // arrange
+    const handA = new Hand();
+    handA.handRank = HandRankType.HighCard;
+    handA.highCardKinds = [13, 10, 8, 7, 6];
+    handA.multiplesCardKinds = [];
+    const handB = new Hand();
+    handB.handRank = HandRankType.HighCard;
+    handB.highCardKinds = [13, 10, 8, 7, 6];
+    handB.multiplesCardKinds = [];
+
+    // act
+    const result = service.compareHands(handA, handB);
+    // assert
+    expect(result).toBe(0);
+  });
+
+  it('should have player A win on high card', () => {
+    // arrange
+    const handA = new Hand();
+    handA.handRank = HandRankType.HighCard;
+    handA.highCardKinds = [13, 10, 8, 7, 6];
+    handA.multiplesCardKinds = [];
+    const handB = new Hand();
+    handB.handRank = HandRankType.HighCard;
+    handB.highCardKinds = [13, 10, 8, 7, 5];
+    handB.multiplesCardKinds = [];
+
+    // act
+    const result = service.compareHands(handA, handB);
+    // assert
+    expect(result).toBe(1);
+  });
 });
